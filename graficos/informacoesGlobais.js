188acbf28685cccf0a327d276546c273c8db297b
Thais Bobato <105284617+thaisbobato@users.noreply.github.com>
2024-10-24 08:41:53 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,19 +1,21 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function visualizarInformacoesGlobais() {
-
     const res = await fetch()
-
     const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
 
     const paragrafo = document.createElement('p')
 paragrafo.classList.add('graficos-container__texto')
-paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas}</span>  horas e <span>${minutos} minutos</span> conectadas.`
         console.log(paragrafo)
 
         const container = document.getElementById('graficos-container')
 container.appendChild(paragrafo)
 }
-const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+
 
 visualizarInformacoesGlobais()
